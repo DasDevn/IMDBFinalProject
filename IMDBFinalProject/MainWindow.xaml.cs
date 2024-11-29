30be996ceed250515a6998eec1b9d50b1add0c6d
Devan Eisnor <W0196059@nscc.ca>
2024-11-29 11:56:26 -0400

    Nuget Dependencies added

--- IMDBFinalProject/MainWindow.xaml.cs
+++ IMDBFinalProject/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+scaffold - dbcontext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Chinook;Integrated Security=True;Trust Server Certificate=False;" Microsoft.EntityFrameworkCore.SqlServer - contextdir Data - outputdir Models / Generated - contextnamespace A2Chinook.Data -namespace A2Chinook.Models -force
 namespace IMDBFinalProject
 {
     /// <summary>
