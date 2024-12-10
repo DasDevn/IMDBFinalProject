e4794239dc3fdb39590cdb8e2e7119839ac3460a
Keegan <62590150+kcolter@users.noreply.github.com>
2024-12-10 19:41:46 -0400

    Working on Artists

--- IMDBFinalProject/Pages/ArtistsPage.xaml.cs
+++ IMDBFinalProject/Pages/ArtistsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IMDBFinalProject.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static IMDBFinalProject.Pages.TVShowsPage;
 
 namespace IMDBFinalProject.Pages
 {
@@ -20,9 +24,49 @@ namespace IMDBFinalProject.Pages
     /// </summary>
     public partial class ArtistsPage : Page
     {
+
+        ImdbContext context = new ImdbContext();
+        CollectionViewSource artistsViewSource = new CollectionViewSource();
         public ArtistsPage()
         {
             InitializeComponent();
+
+            artistsViewSource = (CollectionViewSource)FindResource(nameof(artistsViewSource));
+
+            //get from Names table
+            var artistsResult = context.Names
+                .Select(a => new
+                {
+                    a.NameId, //will be used to get artists works from Titles, via Known_For
+                    a.PrimaryName,
+                    a.BirthYear,
+                    DeathYear = (a.DeathYear != null) ? a.DeathYear.ToString() : "not dead yet"
+                })
+                .ToList(); //using Async as the OrderBy can take some time, and causes program to hang
+
+            //order after the fetch has happened, will hang app too long if done in-fetch
+            //artistsViewSource.Source = artistsResult.OrderBy(a => a.PrimaryName ?? string.Empty); //also checking string.empty, as primary name is a nullable field 
+            artistsViewSource.Source = artistsResult;
         }
+
+        //private void SearchArtists_Click(object sender, RoutedEventArgs e)
+        //{
+        //    using (var context = new ImdbContext())
+        //    {
+        //        var nameToSearch = SearchTextBox.Text.Trim();
+
+        //        var filteredArtists = context.Names
+        //            .Where(a => a.PrimaryName.Contains(nameToSearch) && !a.PrimaryName.IsNullOrEmpty())
+        //            .Select(a => new
+        //            {
+        //                a.NameId, //will be used to get artists works from Titles, via Known_For
+        //                a.PrimaryName,
+        //                a.BirthYear,
+        //                a.DeathYear
+        //            }).ToList();
+
+        //        artistsViewSource.Source = filteredArtists;
+        //    }
+        //}
     }
 }
