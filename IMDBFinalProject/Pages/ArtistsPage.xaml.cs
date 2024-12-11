de4990bf5040283ac0ae3adb649bc4a080ccd6f3
Keegan <62590150+kcolter@users.noreply.github.com>
2024-12-11 05:48:24 -0400

    comments and styling

--- IMDBFinalProject/Pages/ArtistsPage.xaml.cs
+++ IMDBFinalProject/Pages/ArtistsPage.xaml.cs
@@ -51,7 +51,7 @@ namespace IMDBFinalProject.Pages
                             n.BirthYear, //artist year of birth
                             n.DeathYear, //artist year of death
 
-                            ArtistsWorkCount = context.Principals
+                            ArtistsWorkCount = context.Principals //get the count of principals this artist has acted in
                             .Where(p => p.NameId == n.NameId) //where id matches in 
                             .Where(p => //where acting-related job category
                             p.JobCategory != null
@@ -105,18 +105,53 @@ namespace IMDBFinalProject.Pages
                     var nameToSearch = SearchTextBox.Text.Trim();
 
                     var filteredArtists = context.Names
+                        .Take(50) //taking too large a number slows down the speed to read from db
                         .Where(a => a.PrimaryName.Contains(nameToSearch))
-                        .Where(a =>
-                        a.PrimaryProfession.ToLower() == "actor"
-                        || a.PrimaryProfession.ToLower() == "actress")
-                        .Select(a => new
+                        .Where(n => n.PrimaryName != "") //ignore entries with no name
+                        .Where(n =>
+                        n.PrimaryProfession != null //where acting-related job category
+                        || n.PrimaryProfession.ToLower() == "actor"
+                        || n.PrimaryProfession.ToLower() == "actress")
+                        .Select(n => new //select artist info
                         {
-                            a.NameId, //will be used to get artists works from Titles, via Known_For
-                            a.PrimaryName,
-                            a.BirthYear,
-                            a.DeathYear
-                        })
-                        .OrderBy(a => a.PrimaryName)
+                            n.NameId, //will be used when getting related info when user selects given artist
+                            n.PrimaryName, //artist name
+                            n.BirthYear, //artist year of birth
+                            n.DeathYear, //artist year of death
+
+                            ArtistsWorkCount = context.Principals
+                            .Where(p => p.NameId == n.NameId) //where id matches in 
+                            .Where(p => //where acting-related job category
+                            p.JobCategory != null
+                            || p.JobCategory.ToLower() == "actor"
+                            || p.JobCategory.ToLower() == "actress")
+                            .Select(p => p.NameId)
+                            .Count(), //get the count of all valid jobs
+
+                            ArtistsWork = context.Principals //ArtistsWork, holding characters + work character is from
+                            .Where(p => p.NameId == n.NameId) //where id matches in 
+                            .Where(p => //where acting-related job category
+                            p.JobCategory != null
+                        || p.JobCategory.ToLower() == "actor"
+                        || p.JobCategory.ToLower() == "actress")
+                            .Select(p => new
+                            {
+                                Media = context.Titles //media character is from
+                                .Where(t => t.TitleId == p.TitleId && n.NameId == p.NameId) //where titleids match and nameids match
+                                .Where(t => t.PrimaryTitle != null)
+                                .Select(t =>
+                                    t.PrimaryTitle //media title
+                                ).ToList(),
+                                Character = context.Titles //media character is from
+                                .Where(t => t.TitleId == p.TitleId && n.NameId == p.NameId) //where titleids match and nameids match
+                                .Where(t => t.PrimaryTitle != null)
+                                .Select(t =>
+                                    p.Characters //related character
+                                ).ToList()
+                            }).ToList()
+
+                        }).ToList()
+                        .OrderBy(a => a.PrimaryName) //order by name
                         .ToList();
 
                     artistsViewSource.Source = filteredArtists;
