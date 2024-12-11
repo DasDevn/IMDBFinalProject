db857f4065e3b1ed31967056caa76767b28275dd
Keegan <62590150+kcolter@users.noreply.github.com>
2024-12-11 01:21:46 -0400

    implemented try-catch to handle db timeout issues.

--- IMDBFinalProject/Pages/ArtistsPage.xaml.cs
+++ IMDBFinalProject/Pages/ArtistsPage.xaml.cs
@@ -31,53 +31,70 @@ namespace IMDBFinalProject.Pages
         {
             InitializeComponent();
 
+
             artistsViewSource = (CollectionViewSource)FindResource(nameof(artistsViewSource));
 
             using (var context = new ImdbContext())
             {
-                var artists = context.Names
-                    .Where(a => a.PrimaryName != "") //ignore entries with no name
-                    .Where(a => 
-                    a.PrimaryProfession.ToLower() == "actor" 
-                    || a.PrimaryProfession.ToLower() == "actress" 
-                    || a.PrimaryProfession.ToLower() == "self")
-                    .Select(a => new
-                    {
-                        a.NameId, //will be used when getting related info when user selects given artist
-                        a.PrimaryName, //artist name
-                        a.BirthYear,
-                        a.DeathYear
-                    })
-//                    .Take(10000) //TODO: limit to 10000 for dev, find a way to work with full list given reqs
-                    .OrderBy(a => a.PrimaryName) //order by name
-                    .ToList();
-                               
-                artistsViewSource.Source = artists;
+                try
+                {
+                    var artists = context.Names
+                        .Where(a => a.PrimaryName != "") //ignore entries with no name
+                        .Where(a =>
+                        a.PrimaryProfession.ToLower() == "actor"
+                        || a.PrimaryProfession.ToLower() == "actress"
+                        || a.PrimaryProfession.ToLower() == "self")
+                        .Select(a => new
+                        {
+                            a.NameId, //will be used when getting related info when user selects given artist
+                            a.PrimaryName, //artist name
+                            a.BirthYear, //artist year of birth
+                            a.DeathYear //artist year of death
+                        })
+                        .OrderBy(a => a.PrimaryName) //order by name
+                        .ToList();
+
+                    artistsViewSource.Source = artists;
+                }
+                catch (Microsoft.Data.SqlClient.SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
 
            
         }
         private void SearchArtists_Click(object sender, RoutedEventArgs e)
         {
-            using (var context = new ImdbContext())
+            try
             {
-                var nameToSearch = SearchTextBox.Text.Trim();
+                using (var context = new ImdbContext())
+                {
+                    var nameToSearch = SearchTextBox.Text.Trim();
 
-                var filteredArtists = context.Names
-                    .Where(a => a.PrimaryName.Contains(nameToSearch))
-                    .Where(a =>
-                    a.PrimaryProfession.ToLower() == "actor"
-                    || a.PrimaryProfession.ToLower() == "actress"
-                    || a.PrimaryProfession.ToLower() == "self")
-                    .Select(a => new
-                    {
-                        a.NameId, //will be used to get artists works from Titles, via Known_For
-                        a.PrimaryName,
-                        a.BirthYear,
-                        a.DeathYear
-                    }).ToList();
+                    var filteredArtists = context.Names
+                        .Where(a => a.PrimaryName.Contains(nameToSearch))
+                        .Where(a =>
+                        a.PrimaryProfession.ToLower() == "actor"
+                        || a.PrimaryProfession.ToLower() == "actress"
+                        || a.PrimaryProfession.ToLower() == "self")
+                        .Select(a => new
+                        {
+                            a.NameId, //will be used to get artists works from Titles, via Known_For
+                            a.PrimaryName,
+                            a.BirthYear,
+                            a.DeathYear
+                        })
+                        .OrderBy(a => a.PrimaryName)
+                        .ToList();
 
-                artistsViewSource.Source = filteredArtists;
+                    artistsViewSource.Source = filteredArtists;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
